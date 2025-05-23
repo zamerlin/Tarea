b57c1f9d079bff1c71c6c4f311938cfbfe84b0b9
Martínez_Mauro_Gonzalo <maurosaries@gmail.com>
2025-05-23 09:01:48 -0300

    si

--- ultim.py
+++
@@ -1,6 +0,0 @@
-n=int(input("ingrese un numero entero\n"))
-mcua=[None]*n
-for i in range(n):
-    mcua[i] = [0] *n
-    
-print(mcua)
--- ultum.py
+++
@@ -1,7 +0,0 @@
-# Pide al usuario un número entero
-n = int(input("Ingrese un número entero: "))
-
-# Crea una matriz cuadrada de "n" filas y "n" columnas
-macua = [[f"{pri}-{seg}" for seg in range(1, n+1)] for pri in range(1, n+1)]
-
-print((macua))
